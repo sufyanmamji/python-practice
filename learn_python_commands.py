7a01516d61c4a8fafeef07313fea820531c7d101
Sufyan Mamji <sufyan.mamji@gmail.com>
2025-02-26 14:10:21 +0100

    first commit

---
+++ learn_python_commands.py
@@ -0,0 +1,16 @@
+#Python Commands, their explanation and their examples.
+
+#  There are 4 types of variables.
+    
+#Integers are whole numbers without a decimal
+var_integer = 1
+
+#Floats are numbers with decimal.
+var_float = 1.2
+
+#Strings are non numeric values.
+var_strings = "String"
+
+#Booleans are True / False vlues normally used for comparisons.
+var_boolean = True
+
